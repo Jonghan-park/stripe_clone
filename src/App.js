8af415214d1993b28e4cffbd4cfdb4f711c96c61
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-10 09:26:40 -0600

    Create several components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero";
+import Submenu from "./Submenu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
 }
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
