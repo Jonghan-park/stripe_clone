dc6a9c20518a4ebe4c2e4e94ae1860d6380276ad
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-10 12:23:56 -0600

    Update onMouseOver for hero and nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,8 +13,14 @@ const Navbar = () => {
 
     openSubmenu(page, { center, bottom });
   };
+  const handleSubmenu = (e) => {
+    console.log(e.target.classList);
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
